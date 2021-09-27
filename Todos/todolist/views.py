0b9e4dabffa2a584d8c33e2ebe7fa8100c062201
fadallalaaisha <aishafadallala@gmail.com>
2021-09-27 15:48:22 +0300

    Todo_List

--- Todos/todolist/views.py
+++ Todos/todolist/views.py
@@ -2,18 +2,14 @@ from django.shortcuts import render
 from django.http import HttpResponseRedirect
 from .models import TodoItem
 
-
 def todoView(request):
     all_todo_items=TodoItem.objects.all()
-    return render(request, 'todo.html',
-    {'all_items':all_todo_items})
+    return render(request,'todo.html', {'all_items':all_todo_items})
 
 def addTodo(request):
     new_item = TodoItem(content = request.POST['content'])
     new_item.save()
     return HttpResponseRedirect('/todolist/')
-
-   
 
 def deleteTodo(request, todo_id):
     item_delete= TodoItem.objects.get(id=todo_id)
